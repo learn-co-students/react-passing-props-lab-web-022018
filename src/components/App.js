d8d1b41c4c433b537944d25028bffc7614473d00
bafab <bafa@tutanota.de>
2018-04-29 17:31:14 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React from 'react'
+import FruitBasket from './FruitBasket'
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      fruit: [],
+      filters: [],
+      currentFilter: null
+    }
+  }
 
-import FruitBasket from './FruitBasket';
+  componentWillMount() {
+  	    this.fetchFilters();
+  }
 
-const App = () => <FruitBasket />;
+  componentDidMount() {
+      fetch('/api/fruit')
+        .then(x => x.json())
+        .then(json => this.setState({...this.state, fruit: json }))
+  }
 
-export default App;
+  fetchFilters = () => {
+    fetch('/api/fruit_types')
+      .then(x => x.json())
+      .then(json => this.setState({ ...this.state, filters: json }));
+  }
+
+  handleFilterChange = event => {
+      this.setState({ selectedFilter: event.target.value })
+  }
+
+  render() {
+    return (
+      <FruitBasket
+          fruit={this.state.fruit}
+          filters={this.state.filters}
+          currentFilter={this.state.currentFilter}
+          onUpdateFilter={this.updateFilter}
+      />
+    )
+  }
+}
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,34 +1,18 @@
-import React, { Component } from 'react';
-
-class Filter extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      filters: []
-    };
-  }
-
-  componentWillMount() {
-    this.fetchFilters();
-  }
-
-  fetchFilters = () => {
-    fetch('/api/fruit_types')
-      .then(response => response.json())
-      .then(filters => this.setState({ filters }));
-  }
-
-  render() {
+import React from 'react'
+const Filter = (props) => {
     return (
-      <select onChange={this.props.handleChange} defaultValue='all'>
+      <select onChange={props.handleChange} defaultValue='all'>
         <option value='all'>All</option>
-        {this.state.filters.map(filter =>
+        {props.filters.map(filter =>
           <option key={filter} value={filter}>{filter}</option>
         )}
       </select>
-    );
-  }
+    )
 }
 
-export default Filter;
+Filter.defaultProps = {
+  filters: [],
+  handleChange: {undefined}
+}
+
+export default Filter
